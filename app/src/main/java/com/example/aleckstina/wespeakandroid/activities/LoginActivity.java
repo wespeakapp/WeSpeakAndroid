82cf4e720c0782b1398c32ad1a2208566abafcd6
AlecksJohannes <alecksjohanssen@gmail.com>
2017-10-03 15:54:17 +0700

    UI Login Screen complete

---
+++ app/src/main/java/com/example/aleckstina/wespeakandroid/activities/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.aleckstina.wespeakandroid.activities;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.aleckstina.wespeakandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+}
