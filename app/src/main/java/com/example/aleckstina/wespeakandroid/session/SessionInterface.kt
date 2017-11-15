45a5cce1d9195550680176ab663a24f6f52c1277
AlecksJohannes <alecksjohanssen@gmail.com>
2017-11-15 14:09:11 +0700

    Add Session Management

---
+++ app/src/main/java/com/example/aleckstina/wespeakandroid/session/SessionInterface.kt
@@ -0,0 +1,9 @@
+package com.example.aleckstina.wespeakandroid.session
+
+/**
+ * Created by aleckstina on 11/15/17.
+ */
+interface SessionInterface {
+  public fun getAccessToken() : String
+  public fun setAccessToken(accessToken: String)
+}
---
+++ app/src/main/java/com/example/aleckstina/wespeakandroid/session/SessionManagement.kt
@@ -0,0 +1,29 @@
+package com.example.aleckstina.wespeakandroid.session
+
+import android.content.SharedPreferences
+import android.content.Context
+import javax.inject.Singleton
+
+
+/**
+ * Created by aleckstina on 11/15/17.
+ */
+@Singleton
+class SessionManagement(context: Context) : SessionInterface {
+  private var mContext : Context = context
+  private lateinit var mSharedPreferences : SharedPreferences
+  companion object {
+
+  }
+  override fun setAccessToken(accessToken: String) {
+    mSharedPreferences = mContext.getSharedPreferences("", Context.MODE_PRIVATE)
+    val editor = mSharedPreferences.edit()
+    editor.putString("access_token", accessToken)
+    editor.apply()
+  }
+
+  override fun getAccessToken() : String {
+    mSharedPreferences = mContext.getSharedPreferences("", Context.MODE_PRIVATE)
+    return mSharedPreferences.getString("access_token", null)
+  }
+}
